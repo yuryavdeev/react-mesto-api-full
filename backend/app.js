eb8b22756211badff546c663b79bc6d940c0b0d9
yuryavdeev <avdeiev@gmail.com>
2021-08-31 11:08:21 +0300

    remove crash-test

--- backend/app.js
+++ backend/app.js
@@ -36,12 +36,6 @@ app.use(cors);
 
 app.use(requestLogger); // подкл. логгер запросов - перед обработчиками роутов
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 // роуты, не требующие авторизации
 app.post('/signin',
   celebrate({
